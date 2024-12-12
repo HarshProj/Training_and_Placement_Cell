3c0485b751c07ce02dff570f45f8d42e0b16cc66
HarshProj <112640334+HarshProj@users.noreply.github.com>
2024-12-12 23:41:29 +0530

    feat:added adminlogin route at backend

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -26,4 +26,28 @@ router.post('/adminsignup',async(req,res)=>{
     res.status(500).json({ error: error.message || "Internal Server Error" }); 
     }
 })
+router.get('/adminlogin',async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=await Admin.findOne({email:email});
+        if(!user){
+            return res.send("Please Register first");
+        }
+        const cmp=await bcrypt.compare(password,user.password);
+        if(!cmp){
+            return res.send("wrong password");
+        }
+        const data={
+            user:{
+                id:user.id,
+                name:user.name
+            }
+        }
+        const authtoken=jwt.sign(data,hash);
+        res.status(200).json({msg:"Everything is fine",authtoken})
+    } catch (error) {
+        console.log(error); // Correct way to log the error
+    res.status(500).json({ error: error.message || "Internal Server Error" }); 
+    }
+})
 module.exports=router;
