422d4045fc8595b137e31bcde105cd9cd186c3ec
HarshProj <112640334+HarshProj@users.noreply.github.com>
2024-12-14 20:07:18 +0530

    feat:minor bg image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 import {Navbar} from"./Components/Navbar"
-import knit from'./Assets/knit.webp'
+// import knit from'./Assets/knit.webp'
 import Image from 'next/image';
 import {About} from './Components/About'
 import logo from './Assets/logo.png';
+import knit from './Assets/knit.jpeg';
 export default function Home() {
   return (
     <>
     <Navbar/>
-   <div className="  w-full h-[95vh] flex object-contain bg-cover " style={{ background: `url(https://spc.iitj.ac.in/static/media/cdc_pic_2.17b39509.webp) 50%/cover fixed no-repeat`, }}>
-    <div className="text-white bg-transparent h-[250px] w-[600px] m-auto flex flex-col items-center ">
+   <div className="  w-full h-[95vh] flex relative object-contain bg-cover " >
+   <div className="absolute w-full h-full  bg-black" style={{ background: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)),url(${knit.src}) 50%/cover fixed no-repeat`, }}></div>
+    <div className="text-white bg-transparent h-[250px] w-[600px] m-auto flex flex-col items-center    ">
     <div className=" gap-6  flex items-center justify-center w-full h-[70%] ">
 
-<Image className=" h-[100px] w-[100px] bg-cover" src={logo} alt="KNIT logo"  ></Image>
-<div className=" h-full   flex flex-col justify-center gap-2 "><h2 className="text-4xl text-center">Training & Placement Office
+<Image className=" h-[100px] w-[100px] bg-cover backdrop-blur-sm" src={logo} alt="KNIT logo "  ></Image>
+<div className=" h-full   flex flex-col justify-center gap-2 "><h2 className="text-4xl text-center backdrop-blur-sm">Training & Placement Office
 </h2>
 </div>
     </div>
     <div className=" w-[90%] bg-gradient-to-r from-slate-50 to-slate-400 border-t-0 h-[1px] mx-[3px] my-[4px]"></div>
-<h3 className="text-2xl font-light text-center">Kamla Nehru Institute of Technology,Sultanpur</h3>
+<h3 className="text-2xl font-light text-center backdrop-blur-sm">Kamla Nehru Institute of Technology,Sultanpur</h3>
     </div>
    </div>
 
